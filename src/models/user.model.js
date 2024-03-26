40051004c24215c95f43d16fe528502ce479a995
Norphel <113838216+norphel@users.noreply.github.com>
2024-03-26 21:05:04 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  membershipStatus: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
