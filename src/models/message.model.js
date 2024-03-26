f5dbe1f0bad28c4ffac209e6609b68dc10ad700e
Norphel <113838216+norphel@users.noreply.github.com>
2024-03-26 21:05:18 +0530

    add message model

---
+++ src/models/message.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
