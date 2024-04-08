4263e40f282a5e1a77125c96d77a4c15c62e2fbb
Norphel <113838216+norphel@users.noreply.github.com>
2024-04-08 21:42:57 +0530

    validation added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,29 +1,63 @@
 import bcrypt from "bcryptjs";
+import { body, validationResult } from "express-validator";
 
 import { User } from "../models/user.model.js";
 
-const registerUser = async (req, res, next) => {
-  const { firstName, lastName, username, password } = req.body;
+const registerUser = [
+  body("firstName")
+    .trim()
+    .isEmpty()
+    .withMessage("First Name is required")
+    .escape(),
 
-  // validate
-  // check if user already exist
-  //
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  body("lastName")
+    .trim()
+    .isEmpty()
+    .withMessage("Last Name is required")
+    .escape(),
 
-    const user = new User({
-      firstName,
-      lastName,
-      username,
-      password: hashedPassword,
-      membershipStatus: false,
-    });
+  body("username")
+    .trim()
+    .isEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .escape(),
 
-    await user.save();
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    return next(error);
-  }
-};
+  body("username").custom(async (value) => {
+    const existingUser = await User.findOne({ username: username });
+    if (existingUser) {
+      throw new Error("A user with given e-mail already exists!");
+    }
+  }),
+
+  body("password")
+    .trim()
+    .isEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8, max: 24 })
+    .withMessage("Password must be 8-24 characters long")
+    .escape(),
+
+  async (req, res, next) => {
+    const { firstName, lastName, username, password } = req.body;
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = new User({
+        firstName,
+        lastName,
+        username,
+        password: hashedPassword,
+        membershipStatus: false,
+      });
+
+      await user.save();
+      res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
 
 export { registerUser };
